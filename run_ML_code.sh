863f31818c0f13dfd7d861d207306cab814c1b82
nasim <nasimsoltani67@gmail.com>
2021-08-12 10:19:52 -0400

    run_ML_code.sh updated

--- run_ML_code.sh
+++ run_ML_code.sh
@@ -2,15 +2,15 @@
 # ----------------------------------------------------------------------------------------------------
 python -u /home/nasim/UAVFramework/ML_code/top.py \
 --exp_name $1 \
---partition_path /home/nasim/UWBDataSet/PklFiles/Day11_2m_all_orientations/ \
---stats_path /home/nasim/UWBDataSet/PklFiles/Day11_2m_all_orientations/ \
---save_path /home/nasim/CleanFramework/results/ \
---model_flag uwb \
+--partition_path /home/nasim/UAV-TVT/PklFiles/cnn1/ \
+--stats_path /home/nasim/UAV-TVT/PklFiles/cnn1/ \
+--save_path /home/nasim/UAV-TVT/results/ \
+--model_flag alexnet \
 --contin false \
---json_path /home/nasim/CleanFramework/results/dummy/model_file.json \
---hdf5_path /home/nasim/CleanFramework/results/dummy/model.hdf5 \
---slice_size 32 \
---num_classes 4 \
+--json_path /home/nasim/UAV-TVT/results/cnn1/model_file.json \
+--hdf5_path /home/nasim/UAV-TVT/results/cnn1/model.hdf5 \
+--slice_size 200 \
+--num_classes 7 \
 --batch_size 256 \
 --id_gpu $2 \
 --normalize true \
@@ -19,5 +19,5 @@ python -u /home/nasim/UAVFramework/ML_code/top.py \
 --epochs 100 \
 --early_stopping true \
 --patience 5 \
-> /home/nasim/CleanFramework/results/$1/log.out \
-2> /home/nasim/CleanFramework/results/$1/log.err
+> /home/nasim/UAV-TVT/results/$1/log.out \
+2> /home/nasim/UAV-TVT/results/$1/log.err
