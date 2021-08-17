a45468fa713cd1bd6b52fd4409903dd08463155e
nasim <nasimsoltani67@gmail.com>
2021-08-17 14:05:45 -0400

    added exception

--- ML_code/data_generator.py
+++ ML_code/data_generator.py
@@ -20,10 +20,11 @@ class IQDataGenerator(keras.utils.Sequence):
         print('Adding all files to cache')
         for ex in tqdm(self.ex_list):
             this_ex = loadmat(ex)['f_sig']
-            data = np.zeros((this_ex.shape[1],2),dtype=self.args.dtype)
-            data[:,0] = np.real(this_ex[0,:])
-            data[:,1] = np.imag(this_ex[0,:])
-            self.__add_to_cache(ex, data)
+            if this_ex.shape[1] >= self.args.slice_size:
+                data = np.zeros((this_ex.shape[1],2),dtype=self.args.dtype)
+                data[:,0] = np.real(this_ex[0,:])
+                data[:,1] = np.imag(this_ex[0,:])
+                self.__add_to_cache(ex, data)
      
      
     def __len__(self):
@@ -31,7 +32,7 @@ class IQDataGenerator(keras.utils.Sequence):
         with open (os.path.join(self.args.partition_path,'partition.pkl'),'rb') as handle:
             train_list = pkl.load(handle)['train']
         total_slice_cnt = len(train_list)*(self.args.stats['avg_samples']-self.args.slice_size+1)
-        batch_cnt = math.ceil(total_slice_cnt/self.args.batch_size)
+        batch_cnt = int(math.ceil(total_slice_cnt/self.args.batch_size))
         return batch_cnt
 
 
@@ -40,27 +41,27 @@ class IQDataGenerator(keras.utils.Sequence):
        
         if self.args.normalize:
             data = (data - self.args.stats['mean']) / self.args.stats['std']
-        
         self.data_cache[file] = data
     
 
     def __getitem__(self, index):
         #Generate one batch of data 
-        ex_indices = np.random.randint(len(self.ex_list), size=self.args.batch_size)
+        ex_indices = np.random.randint(len(self.data_cache.keys()), size=self.args.batch_size)
         
         X = np.zeros((self.args.batch_size, self.args.slice_size, 2), dtype=self.args.dtype)
         y = np.zeros((self.args.batch_size, self.args.num_classes), dtype=int)
 
         cnt = 0
         for ex_index in ex_indices:
-            this_ex = self.data_cache[self.ex_list[ex_index]]
+            this_ex = self.data_cache[self.data_cache.keys()[ex_index]]
 
             # slicing:
             slice_index = random.randint(0, this_ex.shape[0] - self.args.slice_size - 1)
+
             X[cnt,:,:] = this_ex[slice_index:slice_index+self.args.slice_size, :]
             
             # create y vector:
-            class_index = self.args.device_ids[self.args.labels[self.ex_list[ex_index]]]
+            class_index = self.args.device_ids[self.args.labels[self.data_cache.keys()[ex_index]]]
             y[cnt,class_index] = 1
             
             cnt += 1
--- run_ML_code.sh
+++ run_ML_code.sh
@@ -2,13 +2,13 @@
 # ----------------------------------------------------------------------------------------------------
 python -u /home/nasim/UAVFramework/ML_code/top.py \
 --exp_name $1 \
---partition_path /home/nasim/UAV-TVT/pkl_files/cnn1/ \
---stats_path /home/nasim/UAV-TVT/pkl_files/cnn1/ \
+--partition_path /home/jerry/7_indoor_filtered/ \
+--stats_path /home/jerry/7_indoor_filtered/ \
 --save_path /home/nasim/UAV-TVT/results/ \
 --model_flag alexnet \
 --contin false \
 --json_path /home/nasim/UAV-TVT/results/cnn1/model_file.json \
---hdf5_path /home/nasim/UAV-TVT/results/cnn1/model.hdf5 \
+--hdf5_path /home/nasim/UAV-TVT/results/cnn1/weights.01-0.19.hdf5 \
 --slice_size 200 \
 --num_classes 7 \
 --batch_size 256 \
